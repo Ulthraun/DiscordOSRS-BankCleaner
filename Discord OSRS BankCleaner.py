24986dd515f74d8ee3ec4c1bf0fd0bfea0074c33
Ulthraun <62918284+Ulthraun@users.noreply.github.com>
2020-04-30 16:23:28 -0400

    Add files via upload
    Simply type +b --text --full in discord, download the text file produced by the Old School bot, put it into the same folder as the Bank cleaner, run the bank cleaner, open the text file, copy and paste each into discord. Done~

---
+++ Discord OSRS BankCleaner.py
@@ -0,0 +1,17 @@
+def main():
+    s1 = "+sell "
+    product = {}
+    count = {}
+    with open('Ulthrauns_Bank.txt', 'r') as f:
+        test = f.readline()
+        result = process_line(test)
+        product = [line.strip('\n') for line in f]
+        out = list(map(process_line, product))
+    with open('Ulthrauns_Bank.txt', 'w') as f:
+        f.writelines(["%s\n" % i for i in out])
+    print('Success!')
+def process_line(line):
+    product, count = str(line).split(':')
+    return f'+sell {count} {product} --cf          '
+main()
+
