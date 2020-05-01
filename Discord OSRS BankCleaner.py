a12cc9d93044da143001e9011830ec8aaa190879
Ulthraun <62918284+Ulthraun@users.noreply.github.com>
2020-05-01 08:12:09 -0400

    Update Discord OSRS BankCleaner.py

--- Discord OSRS BankCleaner.py
+++ Discord OSRS BankCleaner.py
@@ -3,15 +3,14 @@ def main():
     product = {}
     count = {}
     with open('Usernames_Bank.txt', 'r') as f:
-        test = f.readline()
-        result = process_line(test)
+        readFile = f.readline()
+        result = process_line(readFile)
         product = [line.strip('\n') for line in f]
         out = list(map(process_line, product))
     with open('Usernames_Bank.txt', 'w') as f:
         f.writelines(["%s\n" % i for i in out])
-    print('Success!')
 def process_line(line):
     product, count = str(line).split(':')
-    return f'+sell {count} {product} --cf          '
+    return f'+sell {count} {product} --cf'
 main()
 
